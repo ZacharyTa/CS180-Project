9db699b807a70211a279dd2d42118410a55a9c28
ZacharyTa <Zachary.C.Ta@gmail.com>
2025-01-17 18:41:04 -0800

    Added daisyui

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,6 +3,7 @@ export default function Test() {
     <div className="container">
       <h1>This is the Test page</h1>
       <p>WOOOO Its working baby</p>
+      <button className="btn btn-primary">DaisyUI works?</button>
     </div>
   );
 }
