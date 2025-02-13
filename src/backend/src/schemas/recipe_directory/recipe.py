f40a0670c9f6c73b785c4294dd40405e996853ee
Moustafa Soliman <moustafa.soliman04@gmail.com>
2025-02-12 18:02:08 -0800

    Implemented Recipe Class.

--- src/backend/src/schemas/recipe_directory/recipe.py
+++ src/backend/src/schemas/recipe_directory/recipe.py
@@ -9,13 +9,31 @@ RECIPE FORMAT:
     Allergens
 """
 
-# Manages individual recipes
+# Manages the information of individual recipes.
 class Recipe:
-    def __init__(self, name, instructions, nutrition_facts, diet):
+    # Initializer for recipe
+    def __init__(self, name, instructions, nutrition_facts, ingredients, diet, image):
         self._name = name
         self._instructions = instructions
         self._nutrition_facts = nutrition_facts
+        self._ingredients = ingredients
         self._diet = diet
+        self._image = image
     
+    def get_name(self):
+        return self._name
     
-
+    def get_instructions(self):
+        return self._instructions
+    
+    def get_nutrition_facts(self):
+        return self._nutrition_facts
+    
+    def get_ingredients(self):
+        return self._ingredients
+    
+    def get_diet(self):
+        return self._diet
+    
+    def get_image(self):
+        return self._image
