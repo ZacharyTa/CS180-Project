53f371ff32f8e617c4d217f6178dcffb27a8e876
Moustafa Soliman <moustafa.soliman04@gmail.com>
2025-02-16 13:31:56 -0800

    Made changes to instructions, allowing steps to be changed.

--- src/backend/src/schemas/recipe_directory/instructions.py
+++ src/backend/src/schemas/recipe_directory/instructions.py
@@ -15,3 +15,9 @@ class Instructions:
             self.__list_of_instructions.remove(step_number - 1)
         except:
             print("Step does not exist!")
+    
+    def change_step(self, step_number, new_step):
+        if isinstance(new_step, str):
+            self[step_number - 1] = new_step
+        else:
+            raise ValueError("New step must be a string!")
